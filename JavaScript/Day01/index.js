068f2f497b2cf155ed00578a154f98af91577a2c
Rahulverma143 <rahulvearma17@gmail.com>
2025-05-30 23:02:04 +0530

    Day01

---
+++ JavaScript/Day01/index.js
@@ -0,0 +1,32 @@
+console.log("Welcome to my first js program!");
+console.log("Rahul verma!");
+console.log("I love js!");
+
+fullName = "Rahul verma ";
+console.log(fullName);
+age = 24;
+console.log(age);
+console.log( typeof fullName); 
+// type checking  (String) 
+
+
+price = 88.55;
+console.log(price);
+console.log( typeof price); 
+// type checking (Number)
+
+x = null; // means empty space
+console.log(x); 
+console.log( typeof x);
+// type check NUll 
+
+y = undefined;  // means no idea  
+console.log(y);
+console.log( typeof y);
+// Type checking undefined 
+
+isFollow = true;    // bollean types value = true or false 
+console.log(isFollow);
+
+isFollow = false;
+console.log(isFollow);
---
+++ JavaScript/Day01/tempCodeRunnerFile.js
@@ -0,0 +1,17 @@
+let user = {
+
+    "user name" : "shreyansh" ,
+    0 : "shreaynsh@gmail.com" ,
+    accountNumber : 1234567890 ,
+    isLoggedinn : ["monday " , "saturday"] ,
+    isMarried : false ,
+
+    country : {
+        state : "rajasthan" ,
+        city : "jaipur" ,
+    }
+
+}
+
+console.log( typeof user);
+console.log(user["user name"]);
