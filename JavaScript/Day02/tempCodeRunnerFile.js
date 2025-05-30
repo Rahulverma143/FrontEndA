742e2252733f45402e53fcaf206d6b3aae819978
Rahulverma143 <rahulvearma17@gmail.com>
2025-05-31 00:11:58 +0530

    updates

--- JavaScript/Day02/tempCodeRunnerFile.js
+++
@@ -1,7 +0,0 @@
-var a = 10     // Old style
-// let b = 20;    // Prferred for non-const
-// const c = 30;  // Preferred for const (cannot be changed)
-
-// console.log(a);  // output 10
-// console.log(b);  // output 20 
-// console.log(c);  // output 30
