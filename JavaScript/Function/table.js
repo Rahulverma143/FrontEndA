cbb991d79d174210d7d8ea5fb3497f4da4d7d6ea
Rahulverma143 <rahulvearma17@gmail.com>
2025-06-04 10:27:37 +0530

    Function

---
+++ JavaScript/Function/table.js
@@ -0,0 +1,11 @@
+// -------Table print------------
+let x = 5;
+for (let i = 1; i <= 10; i++) {
+    console.log(x, "*" ,i, "=", x * i);
+}
+
+// -------------Table print----------------
+let a = 5;
+for (let i = 1; i <= 10; i++) {
+    console.log(a * i);
+}
