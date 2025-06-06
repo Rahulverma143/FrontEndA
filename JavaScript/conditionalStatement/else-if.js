4a9697555d97ef7bf4107ac70c72223bf9bc6aa9
Rahulverma143 <rahulvearma17@gmail.com>
2025-06-06 10:17:31 +0530

    Add practice

---
+++ JavaScript/conditionalStatement/else-if.js
@@ -0,0 +1,24 @@
+//  else-if statement  - multiple condition check
+
+let mode = "dark";
+// let mode = "silver"
+let color;
+
+if(mode === "dark"){
+    color = "black";
+}
+else if (mode === "pink"){
+    color = "pink"
+}
+else if (mode === "blue"){
+    color = "blue"
+}
+else {
+    color = "white"
+}
+    console.log(color);
+
+    // if(mode === "dark")  console.log(mode);
+    //  dark
+    // output blue
+     // output white
---
+++ JavaScript/conditionalStatement/ternary.js
@@ -0,0 +1,14 @@
+//  ternary operator
+// -> MDn weeb docs  search in goolgle
+
+let age = 25;
+
+// age >=18 ? "adult" : "not adult; simpler , compact if-else
+
+let result = age >=18 ? "adult" : "not adult";
+console.log(result);
+//  output  adult
+//  or 
+age >=18 ? console.log("adult") : console.log("not adult");
+
+//  output  adult
