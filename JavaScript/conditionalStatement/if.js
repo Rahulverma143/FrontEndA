68714a53bbcbf7ed5777dcb16deac884a7fcb55b
Rahulverma143 <rahulvearma17@gmail.com>
2025-06-06 08:54:15 +0530

    Conditional Statement

---
+++ JavaScript/conditionalStatement/if.js
@@ -0,0 +1,27 @@
+// Conditional Statement
+//  if Statement
+
+let age = 18;
+
+if(age>=18){
+    console.log("You can vote");  
+}
+if(age<18){
+    console.log("You cannot vote")
+}
+
+//  output  You can vote
+
+
+let mode = "dark";
+let color;
+if(mode === "dark"){
+    color = "black"
+}
+
+if(mode === "light"){
+    color = "white"
+}
+console.log(color)
+
+// outpur black
