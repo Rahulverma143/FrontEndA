7607d9519a85f13044737fac4026d6dd4fc1900c
Rahulverma143 <rahulvearma17@gmail.com>
2025-06-06 09:17:24 +0530

    if-else

---
+++ JavaScript/conditionalStatement/if-else.js
@@ -0,0 +1,45 @@
+// if-else statement 
+
+let mode = "blue";
+let color;
+if(mode === "dark"){
+    color = "black"
+} else {
+color = "white"
+}
+     
+console.log(color)
+
+
+//  output white
+
+// ------------------------------------------------------------
+
+let age = 25;
+// let age = 14;
+
+if(age>=18){
+    console.log("vote")
+}
+else {
+    console.log("not vote")
+}
+
+// output vote
+// output not vote
+
+
+// ----------------------------------------------------------
+
+// let num = 10;
+let num = 15;
+if(num%2 === 0)
+{
+    console.log(num , "is even number");
+}
+else{
+    console.log(num , "is odd number");
+}
+
+//  output  10 is even number
+//  output  15 is odd number
