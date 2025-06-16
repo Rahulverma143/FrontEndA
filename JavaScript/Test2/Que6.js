22dafc4536c8a788b1f5a93a3220f14000396d7e
Rahulverma143 <rahulvearma17@gmail.com>
2025-06-16 16:53:53 +0530

    file

---
+++ JavaScript/Test2/Que6.js
@@ -0,0 +1,11 @@
+// 6. Which of the following correctly defines an arrow function? 
+
+// A. function sum(a, b) => a + b;
+// B. (a, b) => { return a + b; }
+// C. (a, b) -> a + b
+// D. => function(a, b) { return a + b; }
+
+
+
+// output 
+// B. (a, b) => { return a + b; }
