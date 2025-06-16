864e98fb173c5a80dedef44206b530913f97c59d
Rahulverma143 <rahulvearma17@gmail.com>
2025-06-17 00:51:03 +0530

    Que11-12

---
+++ JavaScript/Test2/Que11.js
@@ -0,0 +1,9 @@
+//  11. Which of the following will generate a random integer between 0 and 9?
+
+// A. Math.random() * 10
+// B. Math.floor(Math.random() * 10)
+// C. Math.ceil(Math.random() * 10)
+// D. Math.round(Math.random() * 10)
+
+// output
+// B. Math.floor(Math.random() * 10) 
---
+++ JavaScript/Test2/Que12.js
@@ -0,0 +1,9 @@
+// 12. Which of the following statements best describes an anonymous function?
+
+// A. A function without parameters
+// B. A function without a return value
+// C. A function without a name
+// D. A function that runs only once
+
+// output
+// C. A function without a name
