e12be4d7162e90277dd657b09224c38d447367a2
Rahulverma143 <rahulvearma17@gmail.com>
2025-06-18 10:31:24 +0530

    file

--- JavaScript/importantconcept/Que3.js
+++ JavaScript/importantconcept/Que3.js
@@ -1,1 +1,6 @@
 console.log(!!"Hello");
+
+//  -------------------------------------------------------
+
+console.log( 5 && 1);  // 1  && me last value 
+console.log(5 || 1);  // 5  || me first value
