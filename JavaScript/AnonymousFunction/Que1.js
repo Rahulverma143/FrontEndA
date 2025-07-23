7b01afbb55209bf38075166c9e56798805f53263
Rahulverma143 <rahulvearma17@gmail.com>
2025-07-24 00:09:35 +0530

    add Anonymous Function

---
+++ JavaScript/AnonymousFunction/Que1.js
@@ -0,0 +1,6 @@
+// What is an anonymous function in JavaScript?
+// A function without a name, often used where functions are used temporarily (like callbacks) 
+
+setTimeout(function() {
+  console.log("Hello from anonymous function");
+}, 1000);
