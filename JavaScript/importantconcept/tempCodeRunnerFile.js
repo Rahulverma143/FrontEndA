0604f2f93357055bf90ed467639a23c7442a2ef7
Rahulverma143 <rahulvearma17@gmail.com>
2025-07-26 21:35:11 +0530

    Add Question

--- JavaScript/importantconcept/tempCodeRunnerFile.js
+++ JavaScript/importantconcept/tempCodeRunnerFile.js
@@ -2,3 +2,9 @@ let x = "1";
 // let y = 320;
 // let z = a + b;
 // console.log(z);
+
+function greet(name, callback) {
+  console.log("Hello " + name);
+  callback();
+}
+greet("Rahul", () => console.log("How are you?"));
