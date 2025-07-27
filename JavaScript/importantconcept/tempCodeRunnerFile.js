e1984d7dc29b66d03ebbd67bf33ef15444f4e15c
Rahulverma143 <rahulvearma17@gmail.com>
2025-07-27 21:47:57 +0530

    Add Question

--- JavaScript/importantconcept/tempCodeRunnerFile.js
+++ JavaScript/importantconcept/tempCodeRunnerFile.js
@@ -1,16 +1,24 @@
-let x = "1";
-// let y = 320;
-// let z = a + b;
-// console.log(z);
+// let x = "1";
+// // let y = 320;
+// // let z = a + b;
+// // console.log(z);
 
-function greet(name, callback) {
-  console.log("Hello " + name);
-  callback();
-}
-greet("Rahul", () => console.log("How are you?"));
+// function greet(name, callback) {
+//   console.log("Hello " + name);
+//   callback();
+// }
+// greet("Rahul", () => console.log("How are you?"));
 
 
-let promise = new Promise((resolve, reject) => {
-  setTimeout(() => resolve("Success!"), 1000);
-});
-promise.then(result => console.log(result));
+// let promise = new Promise((resolve, reject) => {
+//   setTimeout(() => resolve("Success!"), 1000);
+// });
+// promise.then(result => console.log(result));
+let intervalId = setInterval(() => {
+  console.log("Repeating...");
+}, 1000);
+
+setTimeout(() => {
+  clearInterval(intervalId);
+  console.log("Stopped interval");
+}, 5000);
