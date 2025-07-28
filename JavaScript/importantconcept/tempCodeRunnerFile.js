1f05842716f898e2a7d1e1a769d2e0f24d35f1e8
Rahulverma143 <rahulvearma17@gmail.com>
2025-07-28 17:07:34 +0530

    Add Question

--- JavaScript/importantconcept/tempCodeRunnerFile.js
+++ JavaScript/importantconcept/tempCodeRunnerFile.js
@@ -39,13 +39,21 @@
 
 
 
- function outer() {
-  let counter = 0;
-  return function inner() {
-    counter++;
-    return counter;
-  }
-}
-const count = outer();
-console.log(count()); // 1
-console.log(count()); // 2
+//  function outer() {
+//   let counter = 0;
+//   return function inner() {
+//     counter++;
+//     return counter;
+//   }
+// }
+// const count = outer();
+// console.log(count()); // 1
+// console.log(count()); // 2
+
+
+// ----------------------------
+
+let p = new Promise((resolve, reject) => {
+  setTimeout(() => resolve("Done!"), 1000);
+});
+p.then(result => console.log(result));
