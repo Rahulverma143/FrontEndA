37550b0894a1052c9c81d754edc579cf79675ae3
Rahulverma143 <rahulvearma17@gmail.com>
2025-07-31 17:01:07 +0530

    dd

--- JavaScript/importantconcept/tempCodeRunnerFile.js
+++ JavaScript/importantconcept/tempCodeRunnerFile.js
@@ -148,19 +148,34 @@
 // arrow function 
  
 
-// 1. No parameters
-const greet = () => console.log("Hello!");
-greet();  // Hello!
+// // 1. No parameters
+// const greet = () => console.log("Hello!");
+// greet();  // Hello!
 
-// 2. One parameter (no parentheses needed)
-const square = x => x * x;
-console.log(square(4)); // 16
+// // 2. One parameter (no parentheses needed)
+// const square = x => x * x;
+// console.log(square(4)); // 16
 
-// 3. Multiple parameters
-const multiply = (a, b) => a * b;
-console.log(multiply(3, 5)); // 15
+// // 3. Multiple parameters
+// const multiply = (a, b) => a * b;
+// console.log(multiply(3, 5)); // 15
 
-// 4. With curly braces (requires return)
-const divide = (a, b) => {
-  return a / b;
-};
+// // 4. With curly braces (requires return)
+// const divide = (a, b) => {
+//   return a / b;
+// };
+
+
+// -------------------------------------
+
+function isPrime(n) {
+  if (n <= 1) return false;
+
+  for (let i = 2; i <= Math.sqrt(n); i++) {
+    if (n % i === 0) return false;
+  }
+  return true;
+}
+
+console.log(isPrime(7)); // true
+console.log(isPrime(10)); // false
