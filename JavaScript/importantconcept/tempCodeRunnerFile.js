d8251ef8150ce6d4562868966d90439d984f82b0
Rahulverma143 <rahulvearma17@gmail.com>
2025-08-01 13:05:05 +0530

    add Que

--- JavaScript/importantconcept/tempCodeRunnerFile.js
+++ JavaScript/importantconcept/tempCodeRunnerFile.js
@@ -209,8 +209,21 @@
 
 // ----------------------
 
-function isPalindrome(str) {
-  return str === str.split('').reverse().join('');
-}
+// function isPalindrome(str) {
+//   return str === str.split('').reverse().join('');
+// }
 
-console.log(isPalindrome("madam")); // true
+// console.log(isPalindrome("madam")); // true
+
+// --------------------
+for (let i = 1; i <= 20; i++) {
+  if (i % 3 === 0 && i % 5 === 0) {
+    console.log("FizzBuzz");
+  } else if (i % 3 === 0) {
+    console.log("Fizz");
+  } else if (i % 5 === 0) {
+    console.log("Buzz");
+  } else {
+    console.log(i);
+  }
+}
