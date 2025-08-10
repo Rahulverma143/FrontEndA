289326de489629e1d45acc5cd03c4bcb3d0a16a9
Rahulverma143 <rahulvearma17@gmail.com>
2025-08-10 21:52:25 +0530

    sd

--- JavaScript/importantconcept/tempCodeRunnerFile.js
+++ JavaScript/importantconcept/tempCodeRunnerFile.js
@@ -383,8 +383,11 @@
 
 // ------------------------------
 
-let fast = new Promise(resolve => setTimeout(() => resolve("Fast"), 1000));
-let slow = new Promise(resolve => setTimeout(() => resolve("Slow"), 3000));
+// let fast = new Promise(resolve => setTimeout(() => resolve("Fast"), 1000));
+// let slow = new Promise(resolve => setTimeout(() => resolve("Slow"), 3000));
 
-Promise.race([fast, slow])
-  .then(result => console.log(result)); // "Fast"
+// Promise.race([fast, slow])
+//   .then(result => console.log(result)); // "Fast"
+
+
+  // --------------------------------------------------------
