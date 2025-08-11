95b29219bd8323ee938a4938f2c1d15ac1a54430
Rahulverma143 <rahulvearma17@gmail.com>
2025-08-11 21:33:26 +0530

    sffhj

--- JavaScript/importantconcept/tempCodeRunnerFile.js
+++ JavaScript/importantconcept/tempCodeRunnerFile.js
@@ -407,13 +407,13 @@
 
 // --------------------------------------
 
-async function test() {
-  console.log("A");
-  await Promise.resolve();
-  console.log("B");
-}
-test();
-console.log("C");
+// async function test() {
+//   console.log("A");
+//   await Promise.resolve();
+//   console.log("B");
+// }
+// test();
+// console.log("C");
 
 
 // ---------------------------------------
