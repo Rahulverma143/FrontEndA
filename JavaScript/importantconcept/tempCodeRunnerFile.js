8849270db77cc4f669305a14045cea9fac1c1f12
Rahulverma143 <rahulvearma17@gmail.com>
2025-08-12 06:09:37 +0530

    djguh

--- JavaScript/importantconcept/tempCodeRunnerFile.js
+++ JavaScript/importantconcept/tempCodeRunnerFile.js
@@ -429,9 +429,12 @@
 
 // ---------------------------
 
-function abc (a){
-  if(a>=40) return
-  console.log(a)
-  abc(a+2)
-}
-abc(1)
+// function abc (a){
+//   if(a>=40) return
+//   console.log(a)
+//   abc(a+2)
+// }
+// abc(1)
+
+
+// -----------------------
