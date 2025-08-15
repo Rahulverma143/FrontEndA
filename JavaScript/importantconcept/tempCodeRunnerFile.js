62b3c76bd63fca765934b7e681f98cf2b05d5dbc
Rahulverma143 <rahulvearma17@gmail.com>
2025-08-15 23:14:40 +0530

    djhddss

--- JavaScript/importantconcept/tempCodeRunnerFile.js
+++ JavaScript/importantconcept/tempCodeRunnerFile.js
@@ -506,19 +506,22 @@
 
 // ---------------------------------------
 
-function isArmstrong(num) {
-    let sum = 0;
-    let temp = num;
-    let digits = num.toString().length;
+// function isArmstrong(num) {
+//     let sum = 0;
+//     let temp = num;
+//     let digits = num.toString().length;
 
-    while (temp > 0) {
-        let digit = temp % 10;
-        sum += digit ** digits;
-        temp = Math.floor(temp / 10);
-    }
+//     while (temp > 0) {
+//         let digit = temp % 10;
+//         sum += digit ** digits;
+//         temp = Math.floor(temp / 10);
+//     }
 
-    return sum === num;
-}
+//     return sum === num;
+// }
 
-console.log(isArmstrong(153)); // true
-console.log(isArmstrong(123)); // false
+// console.log(isArmstrong(153)); // true
+// console.log(isArmstrong(123)); // false
+
+
+// -------------------------------------------
