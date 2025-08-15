6de0a22f7da4e9e5c1d2537046eba97161b0eebc
Rahulverma143 <rahulvearma17@gmail.com>
2025-08-15 23:10:57 +0530

    ff

--- JavaScript/importantconcept/tempCodeRunnerFile.js
+++ JavaScript/importantconcept/tempCodeRunnerFile.js
@@ -464,3 +464,19 @@
 
 // ----------------------------------
 
+let num = 153; // you can change this number
+let sum = 0;
+let temp = num;
+let digits = num.toString().length;
+
+while (temp > 0) {
+    let digit = temp % 10;
+    sum += digit ** digits;
+    temp = Math.floor(temp / 10);
+}
+
+if (sum === num) {
+    console.log(num + " is an Armstrong number");
+} else {
+    console.log(num + " is not an Armstrong number");
+}
