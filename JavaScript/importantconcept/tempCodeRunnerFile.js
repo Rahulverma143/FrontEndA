32f799b8424bb8fe4a23522e2cd34c568e493a17
Rahulverma143 <rahulvearma17@gmail.com>
2025-08-18 22:28:31 +0530

    Ad more

--- JavaScript/importantconcept/tempCodeRunnerFile.js
+++ JavaScript/importantconcept/tempCodeRunnerFile.js
@@ -655,3 +655,15 @@
 
 
 // -----------------------------
+
+let isPrime = (n) => {
+  if (n < 2) return false;
+  for (let i = 2; i <= Math.sqrt(n); i++) {
+    if (n % i === 0) return false;
+  }
+  return true;
+};
+
+let filterPrimes = (arr) => arr.filter(isPrime);
+
+console.log(filterPrimes([2, 3, 4, 5, 10, 11])); // [2, 3, 5, 11]
