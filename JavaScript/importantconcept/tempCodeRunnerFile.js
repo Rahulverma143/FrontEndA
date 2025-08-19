fb76ac5ba8776cc3ce105a47d0fc95394f771bda
Rahulverma143 <rahulvearma17@gmail.com>
2025-08-19 10:54:30 +0530

    updates

--- JavaScript/importantconcept/tempCodeRunnerFile.js
+++ JavaScript/importantconcept/tempCodeRunnerFile.js
@@ -671,5 +671,7 @@
 
 // ------------------------------------------
 
-let countVowels = (str) => str.match(/[aeiou]/gi)?.length || 0;
-console.log(countVowels("javascript")); // 3
+// let countVowels = (str) => str.match(/[aeiou]/gi)?.length || 0;
+// console.log(countVowels("javascript")); // 3
+
+// ----------------------------------------
